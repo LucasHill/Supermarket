5f4633c5d6f76586ecd145be6cf520ef38375260
Lucas Hill <lucas.s.hill@gmail.com>
2015-01-08 01:33:22 -0800

    GID-STY-1702 Add basic test for checkout method, currently failing, as method has yet to be implemented.

--- src/main/java/org/lucashill/example/supermarket/Supermarket.java
+++ src/main/java/org/lucashill/example/supermarket/Supermarket.java
@@ -2,4 +2,9 @@ package org.lucashill.example.supermarket;
 
 public class Supermarket {
 
+    public int checkout(String items) {
+
+        return -1;
+    }
+
 }
--- src/test/java/org/lucashill/example/supermarket/SupermarketTest.java
+++ src/test/java/org/lucashill/example/supermarket/SupermarketTest.java
@@ -1,5 +1,15 @@
 package org.lucashill.example.supermarket;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SupermarketTest {
+    private final static Supermarket supermarket = new Supermarket();
+
+    @Test
+    public void testCheckout() {
+        final int checkoutTotal = supermarket.checkout("ABBACBBAB");
+        Assert.assertEquals(240, checkoutTotal, "Checkout total did not match correct value");
+    }
 
 }
